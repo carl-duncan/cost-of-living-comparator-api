7584169b40526d35f253ab050aba23b2106e38be
Carl Duncan <carl.duncan@gkco.com>
2023-10-19 11:48:44 -0500

    using port 80 instead of 3000

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const cors = require('cors');
 const app = express();
-const PORT = 3001;
+const PORT = 80;
 
 app.use(cors());
 
