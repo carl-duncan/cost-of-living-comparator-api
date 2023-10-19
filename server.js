304a8ff0606679e608fd108c810b9a64269d386a
Carl Duncan <carl.duncan@gkco.com>
2023-10-19 11:36:32 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const cors = require('cors');
+const app = express();
+const PORT = 3001;
+
+app.use(cors());
+
+app.get('/scrape', async (req, res) => {
+    try {
+        const { data } = await axios.get('https://www.numbeo.com/cost-of-living/rankings.jsp');
+        const $ = cheerio.load(data);
+        const citiesAndIndices = [];
+
+        $('tr').each((i, row) => {
+            const city = $(row).find('.cityOrCountryInIndicesTable').text().trim();
+            const index = parseFloat($(row).find('td').eq(2).text().trim());
+
+            if (city && !isNaN(index)) {
+                citiesAndIndices.push({ city, index });
+            }
+        });
+
+        res.json(citiesAndIndices);
+    } catch (error) {
+        res.status(500).send('Error fetching data' + error);
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
